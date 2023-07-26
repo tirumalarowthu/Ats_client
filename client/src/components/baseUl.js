4a83ef35ad28a61d24a7dc43f4eff879f91e465f
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-07-26 12:35:57 +0530

    runner in github added

--- client/src/components/baseUl.js
+++ client/src/components/baseUl.js
@@ -1,4 +1,6 @@
 // export const baseUrl = "http://13.233.142.85"
-export const baseUrl = "http://localhost:9005"
+// export const baseUrl = "http://localhost:9005"
+export const baseUrl = ""
+
 export const TES_URL="http://localhost:7001"
 // export const baseUrl = ""
