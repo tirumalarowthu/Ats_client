733433245db32cf0a766adec1cc506fa55a33ed5
Satyanarayana Tirumalarowthu <7395@Tessolveindia.com>
2024-07-02 17:59:34 +0530

    new requirement

--- client/src/components/baseUl.js
+++ client/src/components/baseUl.js
@@ -1,7 +1,8 @@
 // export const baseUrl = "http://13.233.142.85"
-export const baseUrl = "http://13.233.161.128" ///for local system
+export const baseUrl = "http://localhost:9005" ///for local system
+// export const baseUrl = "http://13.233.161.128" ///for local system
 // export const baseUrl = ""
 
 // export const TES_URL="http://13.233.116.160" ///for test system url
-export const TES_URL="http://13.233.90.50" ///for test system url locally
+export const TES_URL="https://online-tes.com" ///for test system url locally
 // export const baseUrl = ""
